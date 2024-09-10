3ed7aa56895d27dfbb35692cca4797ad649e9a1d
Vladimir Chernov <vladimir120905@gmail.com>
2024-09-10 10:13:29 +0300

    Upload Task2

---
+++ src/lab2/Task2.java
@@ -0,0 +1,12 @@
+package lab2;
+
+// HowMany.java
+public class Task2 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (String arg: args) {
+            count++;
+        }
+        System.out.println("Вы ввели в консоль " + count + " слов.");
+    }
+}
